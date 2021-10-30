3926fe46768a82ced29ed186c713cb9b6a07ff4c
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-30 17:38:35 +0800

    category.blade

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -50,8 +50,14 @@
 
 <div class="container">
     <div class="starter-template">
-                            <h1>
-        Мобильные телефоны 5
+    <h1>
+        @if($category == 'mobiles')
+        Мобильные телефоны 
+        @elseif($category == 'portable')
+        Портативная техника
+        @elseif($category == 'appliances')
+        Бытовая техника
+        @endif
     </h1>
     <p>
         В этом разделе вы найдёте самые популярные мобильные телефонамы по отличным ценам!
