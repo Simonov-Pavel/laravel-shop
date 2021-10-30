a418fe0564ecd3311ddb8117ef55cbe695e9657f
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-30 17:28:35 +0800

    link category

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -49,7 +49,7 @@
 <div class="container">
     <div class="starter-template">
                                     <div class="panel">
-            <a href="mobiles.html">
+            <a href="/mobiles">
                 <img src="http://internet-shop.tmweb.ru/storage/categories/mobile.jpg">
                 <h2>Мобильные телефоны</h2>
             </a>
@@ -58,7 +58,7 @@
             </p>
         </div>
             <div class="panel">
-            <a href="portable.html">
+            <a href="/portable">
                 <img src="http://internet-shop.tmweb.ru/storage/categories/portable.jpg">
                 <h2>Портативная техника</h2>
             </a>
@@ -67,7 +67,7 @@
             </p>
         </div>
             <div class="panel">
-            <a href="appliances.html">
+            <a href="/appliances">
                 <img src="http://internet-shop.tmweb.ru/storage/categories/appliance.jpg">
                 <h2>Бытовая техника</h2>
             </a>
