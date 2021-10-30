996aecfa4247e925d513ca5835b744df7ecd46e0
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-30 20:10:23 +0800

    Route.bascet

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ Route::get('/', [MainController::class, 'index'])->name('index');
 Route::get('/categories', [MainController::class, 'categories'])->name('categories');
 Route::get('/{category}', [MainController::class, 'category'])->name('category');
 Route::get('/{category}/{product}', [MainController::class, 'product'])->name('product');
+Route::get('/bascet', [MainController::class, 'bascet'])->name('bascet');
