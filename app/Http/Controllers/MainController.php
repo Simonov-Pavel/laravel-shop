b9b824fc59aa82a522e814ba456dfe4eb469fd56
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-30 17:15:04 +0800

    Main Controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    public function index(){
+        return view('welcome');
+    }
+
+    public function categories(){
+        return view('categories');
+    }
+
+    public function category($category){
+        return '';
+    }
+
+    public function product($category, $product){
+        return view('product', compact('category', 'product'));
+    }
 }
