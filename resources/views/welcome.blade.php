f8a7ab4c8acc9413b4d6fcdf5c1018c966340f85
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-30 19:46:00 +0800

    views.welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,51 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-    <title>Интернет Магазин: Главная</title>
+@section('title', 'Главная')
 
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/starter-template.css" rel="stylesheet">
-</head>
-<body>
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">Интернет Магазин</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Все товары</a></li>
-                <li ><a href="categories.html">Категории</a>
-                </li>
-                <li ><a href="basket.html">В корзину</a></li>
-                <li><a href="http://internet-shop.tmweb.ru/reset">Сбросить проект в начальное состояние</a></li>
-                <li><a href="http://internet-shop.tmweb.ru/locale/en">en</a></li>
-
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">₽<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                                                    <li><a href="http://internet-shop.tmweb.ru/currency/RUB">₽</a></li>
-                                                    <li><a href="http://internet-shop.tmweb.ru/currency/USD">$</a></li>
-                                                    <li><a href="http://internet-shop.tmweb.ru/currency/EUR">€</a></li>
-                                            </ul>
-                </li>
-            </ul>
-
-            <ul class="nav navbar-nav navbar-right">
-                                    <li><a href="http://internet-shop.tmweb.ru/login">Войти</a></li>
-                
-                            </ul>
-        </div>
-    </div>
-</nav>
-
+@section('content')
 <div class="container">
     <div class="starter-template">
                             <h1>Все товары</h1>
@@ -96,7 +53,7 @@
             </p>
         </div>
     </div>
-</div>
+    </div>
                     <div class="col-sm-6 col-md-4">
     <div class="thumbnail">
         <div class="labels">
@@ -117,7 +74,7 @@
             </p>
         </div>
     </div>
-</div>
+    </div>
                     <div class="col-sm-6 col-md-4">
     <div class="thumbnail">
         <div class="labels">
@@ -138,7 +95,7 @@
             </p>
         </div>
     </div>
-</div>
+    </div>
                     <div class="col-sm-6 col-md-4">
     <div class="thumbnail">
         <div class="labels">
@@ -159,7 +116,7 @@
             </p>
         </div>
     </div>
-</div>
+    </div>
                     <div class="col-sm-6 col-md-4">
     <div class="thumbnail">
         <div class="labels">
@@ -180,7 +137,7 @@
             </p>
         </div>
     </div>
-</div>
+    </div>
                     <div class="col-sm-6 col-md-4">
     <div class="thumbnail">
         <div class="labels">
@@ -202,20 +159,18 @@
             </p>
         </div>
     </div>
-</div>
+    </div>
             </div>
     <nav>
         <ul class="pagination">
-                <li class="page-item disabled" aria-disabled="true" aria-label="pagination.previous">
-                    <span class="page-link" aria-hidden="true">&lsaquo;</span>
-                </li>
-                <li class="page-item">
-                    <a class="page-link" href="?&amp;page=2" rel="next" aria-label="pagination.next">&rsaquo;</a>
-                </li>
-                    </ul>
+            <li class="page-item disabled" aria-disabled="true" aria-label="pagination.previous">
+                <span class="page-link" aria-hidden="true">&lsaquo;</span>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="?&amp;page=2" rel="next" aria-label="pagination.next">&rsaquo;</a>
+            </li>
+        </ul>
     </nav>
 
     </div>
-</div>
-</body>
-</html>
+@endsection
