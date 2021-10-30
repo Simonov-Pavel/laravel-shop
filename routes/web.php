bce32b2ab0104624e217b9d7e8603a062a1243ff
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-30 20:37:35 +0800

    route.order

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\MainController;
 
 Route::get('/', [MainController::class, 'index'])->name('index');
 Route::get('/categories', [MainController::class, 'categories'])->name('categories');
+Route::get('/bascet', [MainController::class, 'bascet'])->name('bascet');
+Route::get('/order', [MainController::class, 'order'])->name('order');
 Route::get('/{category}', [MainController::class, 'category'])->name('category');
 Route::get('/{category}/{product}', [MainController::class, 'product'])->name('product');
-Route::get('/bascet', [MainController::class, 'bascet'])->name('bascet');
+
