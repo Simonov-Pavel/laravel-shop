aaf1b9237b4ddc4f2bef1c10661dc2c316bef6a1
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-30 19:55:57 +0800

    views.category

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,75 +1,26 @@
+@extends('layouts.main')
 
+@section('title', $category->name)
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Online Shop: Category Мобильные телефоны</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <script src="/js/jquery.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/starter-template.css" rel="stylesheet">
-</head>
-<body>
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="http://internet-shop.tmweb.ru">Online Shop</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li ><a href="http://internet-shop.tmweb.ru">All products</a></li>
-                <li class="active"><a href="http://internet-shop.tmweb.ru/categories">Categories</a>
-                </li>
-                <li ><a href="http://internet-shop.tmweb.ru/basket">Your cart</a></li>
-                <li><a href="http://internet-shop.tmweb.ru/reset">Reset project to default state</a></li>
-                <li><a href="http://internet-shop.tmweb.ru/locale/ru">ru</a></li>
-
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">₽<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                                                    <li><a href="http://internet-shop.tmweb.ru/currency/RUB">₽</a></li>
-                                                    <li><a href="http://internet-shop.tmweb.ru/currency/USD">$</a></li>
-                                                    <li><a href="http://internet-shop.tmweb.ru/currency/EUR">€</a></li>
-                                            </ul>
-                </li>
-            </ul>
-
-            <ul class="nav navbar-nav navbar-right">
-                                    <li><a href="http://internet-shop.tmweb.ru/login">Login</a></li>
-                
-                            </ul>
-        </div>
-    </div>
-</nav>
-
+@section('content')
 <div class="container">
     <div class="starter-template">
     <h1>{{$category->name}}</h1>
     <p>{{$category->description}}</p>
     <div class="row">
-                    <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-        <div class="labels">
-            
-            
-                    </div>
-        <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg" alt="iPhone X 64GB">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <div class="labels"></div>
+                    <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg" alt="iPhone X 64GB">
         <div class="caption">
             <h3>iPhone X 64GB</h3>
             <p>71990 ₽</p>
             <p>
-            <form action="http://internet-shop.tmweb.ru/basket/add/1" method="POST">
-                                    <button type="submit" class="btn btn-primary" role="button">Add to cart</button>
-                                <a href="http://internet-shop.tmweb.ru/mobiles/iphone_x_64"
-                   class="btn btn-default"
-                   role="button">More about</a>
-                <input type="hidden" name="_token" value="Au89njmajixRjy3GRhAzqETFIKINQndX9YjsdiUz">            </form>
+                <form action="http://internet-shop.tmweb.ru/basket/add/1" method="POST">
+                    <button type="submit" class="btn btn-primary" role="button">Add to cart</button>
+                    <a href="http://internet-shop.tmweb.ru/mobiles/iphone_x_64"class="btn btn-default" role="button">More about</a>
+                    <input type="hidden" name="_token" value="Au89njmajixRjy3GRhAzqETFIKINQndX9YjsdiUz">            
+                </form>
             </p>
         </div>
     </div>
@@ -160,6 +111,4 @@
 </div>
             </div>
     </div>
-</div>
-</body>
-</html>
+@endsection
