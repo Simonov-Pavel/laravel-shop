cb08cfb9ac5891a5de18454bf21917f6bd574650
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-30 17:24:31 +0800

    category.blade

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 Route::get('/', [MainController::class, 'index']);
 Route::get('/categories', [MainController::class, 'categories']);
