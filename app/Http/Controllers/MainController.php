6cb829a58424c750640fb3f7a1386cd47fa15188
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-31 14:03:24 +0800

    Получение всех продуктов из базы данных

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Product;
 
 class MainController extends Controller
 {
     public function index(){
+        $products = Product::get();
         return view('welcome');
     }
 
