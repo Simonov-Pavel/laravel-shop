71775cb64d64498224aea9ddec1abce76161b89e
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-31 19:03:33 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,11 @@ use App\Http\Controllers\BasketController;
 ;
 
 Route::get('/', [MainController::class, 'index'])->name('index');
-Route::get('/categories', [MainController::class, 'categories'])->name('categories');
+
 Route::get('/bascet', [BasketController::class, 'bascet'])->name('bascet');
-Route::get('/order', [BasketController::class, 'order'])->name('order');
+Route::get('/bascet/order', [BasketController::class, 'order'])->name('order');
+
+Route::get('/categories', [MainController::class, 'categories'])->name('categories');
 Route::get('/{category}', [MainController::class, 'category'])->name('category');
 Route::get('/{category}/{product}', [MainController::class, 'product'])->name('product');
 
