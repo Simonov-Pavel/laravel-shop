cfe6c021288211a56f6215cf8fd6dd350cf14487
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-31 21:27:07 +0800

    Route для удаления товара из корзины

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/', [MainController::class, 'index'])->name('index');
 Route::get('/bascet', [BasketController::class, 'bascet'])->name('bascet');
 Route::get('/bascet/order', [BasketController::class, 'order'])->name('order');
 Route::post('/bascet/add/{id}', [BasketController::class, 'bascetAdd'])->name('bascet-add');
+Route::post('/bascet/remove/{id}', [BasketController::class, 'bascetRemove'])->name('bascet-remove');
 
 Route::get('/categories', [MainController::class, 'categories'])->name('categories');
 Route::get('/{category}', [MainController::class, 'category'])->name('category');
