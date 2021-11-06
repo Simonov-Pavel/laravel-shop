5a50559d97623c2ab346cf02959ef45ce9c1a601
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-06 11:23:10 +0800

    Route bascet-confirm

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Route::get('/bascet', [BasketController::class, 'bascet'])->name('bascet');
 Route::get('/bascet/order', [BasketController::class, 'order'])->name('order');
 Route::post('/bascet/add/{id}', [BasketController::class, 'bascetAdd'])->name('bascet-add');
 Route::post('/bascet/remove/{id}', [BasketController::class, 'bascetRemove'])->name('bascet-remove');
+Route::post('/bascet/order', [BasketController::class, 'bascetConfirm'])->name('bascet-confirm');
 
 Route::get('/categories', [MainController::class, 'categories'])->name('categories');
 Route::get('/{category}', [MainController::class, 'category'])->name('category');
