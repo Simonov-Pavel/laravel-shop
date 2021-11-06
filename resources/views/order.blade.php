90a7cd0726d3a90216ae51e95915ac19e7d1e8ae
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-06 12:37:02 +0800

    value old

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -15,7 +15,7 @@
                         <div class="form-group">
                             <label for="name" class="control-label col-lg-offset-3 col-lg-2">Имя: </label>
                             <div class="col-lg-4">
-                                <input type="text" name="name" id="name" value="" class="form-control">
+                                <input type="text" name="name" id="name" value="{{old('name')}}" class="form-control">
                             </div>
                             @error('name')
 						<div class="text-danger">{{$message}}</div>
@@ -26,7 +26,7 @@
                         <div class="form-group">
                             <label for="phone" class="control-label col-lg-offset-3 col-lg-2">Номер телефона: </label>
                             <div class="col-lg-4">
-                                <input type="text" name="phone" id="phone" value="" class="form-control">
+                                <input type="text" name="phone" id="phone" value="{{old('phone')}}" class="form-control">
                             </div>
                             @error('phone')
 						        <div class="text-danger">{{$message}}</div>
