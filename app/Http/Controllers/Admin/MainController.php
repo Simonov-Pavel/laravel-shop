8e48cef0675b9a29cd9686c337c79c07601046fd
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-13 13:33:57 +0800

    Получение заказов

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Order;
 
 class MainController extends Controller
 {
     public function index(){
-        return view('admin.index');
+        $orders = Order::where('status', '1')->get();
+        
+        return view('admin.index', compact('orders'));
     }
 }
