54377d451c4639496d52a8eaddb1cdf5c9f1450b
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-13 14:44:38 +0800

    orders.blade

---
+++ resources/views/admin/orders.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.panel')
+@section('title', 'Админ панель - Заказы')
+@section('menu')
+@include('includes.admin-menu')
+@endsection
+@section('content')
+
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Заказы</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route('admin') }}">Главная</a></li>
+                        <li class="breadcrumb-item active">Заказы</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card-body p-0">
+        <table class="table table-hover text-nowrap">
+            <thead>
+                <tr>
+                    <th style="width: 10px">№</th>
+                    <th>Пользователь</th>
+                    <th>Телефон</th>
+                    <th>Сумма заказа</th>
+                    <th>Действие</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($orders as $order)
+                <tr>
+                    <td>{{$order->id}}</td>
+                    <td>{{$order->name}}</td>
+                    <td>{{$order->phone}}</td>
+                    <td>{{$order->getFullPrice()}}</td>
+                    <td>
+                        <a href="#" class="btn btn-tool"><i class="fas fa-eye text-success"></i></a>
+                        <a href="#" class="btn btn-tool"><i class="fas fa-pen text-primary"></i></a>
+                        <a href="#" class="btn btn-tool"><i class="fas fa-times text-danger"></i></a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
--- resources/views/includes/admin-menu.blade.php
+++ resources/views/includes/admin-menu.blade.php
@@ -1,9 +1,9 @@
 <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <li class="nav-item">
-            <a href="pages/gallery.html" class="nav-link">
+            <a href="{{ route('admin.orders') }}" class="nav-link">
                 <i class="nav-icon far fa-image"></i>
-                <p>Gallery</p>
+                <p>Заказы</p>
             </a>
         </li>
     </ul>
