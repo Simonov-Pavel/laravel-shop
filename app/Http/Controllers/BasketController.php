7cffbfa1127e77bf3bdb0d21c1dcff1022eb19a2
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-17 18:11:54 +0800

    php artisan make:request CategoryRequest

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Request\OrderRequest;
+use App\Http\Requests\OrderRequest;
 use App\Models\Order;
 use App\Models\Product;
 
