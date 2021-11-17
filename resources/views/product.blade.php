63ee7dd48e7939dc8f7d0d132e6a3b661f2dc8a1
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-17 16:53:08 +0800

    show image product.blade

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.main')
 
-@section('title', 'Продукт ')
+@section('title', 'Продукт ' . $product->name)
 
 @section('content')
 
         <h1>{{$product->name}}</h1>
         <h2>{{$product->category->name}}</h2>
         <p>Price: <b>{{$product->price}} ₽</b></p>
-        <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x_silver.jpg">
+        <img src="{{ Storage::url($product->image) }}">
         <p>{{$product->description}}</p>
         <form action="{{route('bascet-add', $product)}}" method="POST">
             @csrf
