eae45a8e61f27a63133df9a1d2582cd530e91ff2
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-17 15:01:33 +0800

    Получение всех продуктов

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Order;
+use App\Models\Product;
 
 class MainController extends Controller
 {
     public function index(){
         $orders = Order::where('status', '1')->get();
-        
-        return view('admin.index', compact('orders'));
+        $products = Product::get();
+        return view('admin.index', compact('orders', 'products'));
     }
 }
