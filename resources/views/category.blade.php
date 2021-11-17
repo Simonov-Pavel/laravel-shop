1e3e662b2b5b5472360349e00b3ae7c37f5696e6
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-17 10:32:18 +0800

    Условие вывода продуктов в категории

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -7,8 +7,12 @@
     <h1>{{$category->name}}</h1>
     <p>{{$category->description}}</p>
     <div class="row">
-        @foreach($category->products as $product)
-            @include('includes.cart-product', compact('product'))
-        @endforeach
+        @if($category->products->count() > 0)
+            @foreach($category->products as $product)
+                @include('includes.cart-product', compact('product'))
+            @endforeach
+        @else
+            <h3>В этой категории пока нет продуктов... </h3>
+        @endif
     </div>
 @endsection
