df9d86861d32c7496b24001381ae6cbe99dd70b8
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-18 16:18:09 +0800

    create resource controller OrderController with model

--- resources/views/includes/user-menu.blade.php
+++ resources/views/includes/user-menu.blade.php
@@ -1,13 +1,13 @@
 <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <li class="nav-item">
-            <a href="{{ route('admin') }}" class="@menuactive('user') nav-link">
+            <a href="{{ route('user') }}" class="@menuactive('user') nav-link">
             <i class="nav-icon ion ion-bag"></i>
                 <p>Главная</p>
             </a>
         </li>
         <li class="nav-item">
-            <a href="{{ route('admin.orders') }}" class="@menuactive('admin.order*') nav-link">
+            <a href="" class="@menuactive('user.order*') nav-link">
             <i class="nav-icon ion ion-bag"></i>
                 <p>Заказы</p>
             </a>
