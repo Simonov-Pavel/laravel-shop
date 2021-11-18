63c7f8c4e0341d099f1cb6f911065a989de27d44
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-18 13:49:28 +0800

    подсветка пункта меню админ панели

--- resources/views/includes/admin-menu.blade.php
+++ resources/views/includes/admin-menu.blade.php
@@ -1,19 +1,25 @@
 <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <li class="nav-item">
-            <a href="{{ route('admin.orders') }}" class="nav-link">
+            <a href="{{ route('admin') }}" class="@menuactive('admin') nav-link">
+            <i class="nav-icon ion ion-bag"></i>
+                <p>Главная</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('admin.orders') }}" class="@menuactive('admin.order*') nav-link">
             <i class="nav-icon ion ion-bag"></i>
                 <p>Заказы</p>
             </a>
         </li>
         <li class="nav-item">
-            <a href="{{ route('categories.index') }}" class="nav-link">
+            <a href="{{ route('categories.index') }}" class="@menuactive('categories*') nav-link">
             <i class="nav-icon ion ion-bag"></i>
                 <p>Категории</p>
             </a>
         </li>
         <li class="nav-item">
-            <a href="{{ route('products.index') }}" class="nav-link">
+            <a href="{{ route('products.index') }}" class="@menuactive('products*') nav-link">
             <i class="nav-icon ion ion-bag"></i>
                 <p>Продукты</p>
             </a>
