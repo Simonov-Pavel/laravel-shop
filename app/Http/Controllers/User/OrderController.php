b33ac9779f95be084dcec1f0a4ea3c3d2668cace
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-18 16:19:14 +0800

    User/OrderController

--- app/Http/Controllers/User/OrderController.php
+++ app/Http/Controllers/User/OrderController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Order;
 use Illuminate\Http\Request;
 
-class OrdrController extends Controller
+class OrderController extends Controller
 {
     /**
      * Display a listing of the resource.
