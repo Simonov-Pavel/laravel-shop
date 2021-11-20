97b5f9205f0079508b84e1295c2d644151ff9430
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-20 20:45:58 +0800

    labels in cart-product

--- resources/views/includes/cart-product.blade.php
+++ resources/views/includes/cart-product.blade.php
@@ -1,6 +1,16 @@
 <div class="col-sm-6 col-md-4">
     <div class="thumbnail">
-        <div class="labels"></div>
+        <div class="labels">
+            @if($product->isNew())
+                <span class="badge badge-success">Новинка</span>
+            @endif
+            @if($product->isHit())
+                <span class="badge badge-warning">Хит</span>
+            @endif
+            @if($product->isRecomend())
+                <span class="badge badge-danger">Рекомендуем</span>
+            @endif
+        </div>
         <img src="{{Storage::url($product->image)}}">
         <div class="caption">
             <h3>{{$product->name}}</h3>
