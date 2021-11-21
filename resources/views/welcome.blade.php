e5fd70d1f1b7e558ed24a0338824bc07adc705b8
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-21 11:32:10 +0800

    links pagination on main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,15 +41,6 @@
             @include('includes.cart-product', compact('product'))
         @endforeach
     </div>
-        <nav>
-            <ul class="pagination">
-                <li class="page-item disabled" aria-disabled="true" aria-label="pagination.previous">
-                    <span class="page-link" aria-hidden="true">&lsaquo;</span>
-                </li>
-                <li class="page-item">
-                    <a class="page-link" href="?&amp;page=2" rel="next" aria-label="pagination.next">&rsaquo;</a>
-                </li>
-            </ul>
-        </nav>
-
+    {{$products->links()}}
+    
 @endsection
