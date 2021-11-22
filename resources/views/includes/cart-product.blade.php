628b70212c8722ea50a36401fd75a0d3984f9429
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-22 13:43:45 +0800

    cart-product

--- resources/views/includes/cart-product.blade.php
+++ resources/views/includes/cart-product.blade.php
@@ -18,7 +18,11 @@
             <p>
                 <form action="{{route('bascet-add', $product)}}" method="POST">
                     @csrf
-                    <button type="submit" class="btn btn-primary" role="button">В корзину</button>
+                    @if($product->isAvailable())
+                        <button type="submit" class="btn btn-primary" role="button">В корзину</button>
+                    @else
+                        Недоступен для заказа
+                    @endif
                     <a href="{{route('product', [isset($category) ? $category->code : $product->category->code, $product->code])}}" class="btn btn-default" role="button">Подробнее</a>
                 </form>
                 
