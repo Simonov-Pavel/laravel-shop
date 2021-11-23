6ada010d6b1a59bb7d2a3f1e01d7d672784ed690
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-23 19:08:10 +0800

    OrderCreated

--- app/Mail/OrderCreated.php
+++ app/Mail/OrderCreated.php
@@ -6,20 +6,23 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use App\Models\Order;
 
 class OrderCreated extends Mailable
 {
     use Queueable, SerializesModels;
 
-
+    protected $name;
+    protected $order;
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($name, Order $order)
     {
-        //
+        $this->name = $name;
+        $this->order = $order;
     }
 
     /**
@@ -29,6 +32,11 @@ class OrderCreated extends Mailable
      */
     public function build()
     {
-        return $this->view('mail.order-created');
+        $fullSum = $this->order->calculateFullPrice();
+        return $this->view('mail.order-created', [
+            'name' => $this->name,
+            'order' => $this->order,
+            'fullSum' => $fullSum
+        ]);
     }
 }
