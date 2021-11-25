93da47bacf4a15637cd7ac1d16a079cbd00c3183
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-25 12:27:28 +0800

    верстка письма

--- resources/views/mail/order-created.blade.php
+++ resources/views/mail/order-created.blade.php
@@ -3,23 +3,23 @@
 <p>Спасибо что выбрали наш магазин</p>
 
 <p>Ваш заказ :</p>
-<table style="width:100%">
-        <tr style="width:100%">
-            <th>Название</th>
-            <th>Кол-во</th>
-            <th>Цена</th>
+<table width="100%">
+        <tr>
+            <td height="100%">Название</td>
+            <td height="100%">Кол-во</td>
+            <td height="100%">Цена</td>
         </tr>
     <hr>
     @foreach($order->products()->with('category')->get() as $product)
         <tr style="width:100%">
-            <td>{{$product->name}}</td>
-            <td style="text-align:center;">{{ $product->pivot->count }}</td>
-            <td>{{$product->price}} ₽</td>
+            <td height="100%">{{$product->name}}</td>
+            <td height="100%">{{ $product->pivot->count }}</td>
+            <td height="100%">{{$product->price}} ₽</td>
         </tr>
         <hr>
     @endforeach
         <tr style="width:100%">
-            <td>Общая стоимость:</td>
-            <td>{{$fullSum}} ₽</td>
+            <td height="100%">Общая стоимость:</td>
+            <td height="100%">{{$fullSum}} ₽</td>
         </tr>
 </table>
