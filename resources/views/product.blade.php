43691c9c9ac05cf4a7d65ce69ab37664f776857c
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-25 12:41:45 +0800

    Форма уведомления о поступлении товара

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -9,14 +9,21 @@
         <p>Price: <b>{{$product->price}} ₽</b></p>
         <img src="{{ Storage::url($product->image) }}">
         <p>{{$product->description}}</p>
-        <form action="{{route('bascet-add', $product)}}" method="POST">
-            @csrf
-            @if($product->isAvailable())
+        @if($product->isAvailable())
+            <form action="{{route('bascet-add', $product)}}" method="POST">
+                @csrf
                 <button type="submit" class="btn btn-success" role="button">В корзину</button>
-             @else
-                <b>Недоступен для заказа</b>
-            @endif
+            </form>
+        @else
+            <span><h3>Недоступен для заказа</h3></span><br>
+            <span><b>Сообщить мне о поступлении этого товара</b></span><br>
+             <form action="" method="post">
+                 @csrf
+                 <input type="email" name="email">
+                 <button type="submit">Отравить</button>
+             </form>
+        @endif
                   
-        </form>
+        
 
 @endsection
