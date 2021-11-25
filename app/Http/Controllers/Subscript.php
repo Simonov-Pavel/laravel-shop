94add89a6e156ed3ccea40130805f01c4fc72fa0
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-11-25 13:13:53 +0800

    create controller Subscript

---
+++ app/Http/Controllers/Subscript.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subscription;
+use Illuminate\Http\Request;
+
+class Subscript extends Controller
+{
+    public function store(Request $request)
+    {
+        //
+    }
+}
