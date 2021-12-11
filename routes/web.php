25f12b1adfb2a589c142fb7c31120df3df90fa6a
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-12-11 11:27:07 +0800

    web.php

--- routes/web.php
+++ routes/web.php
@@ -11,43 +11,43 @@ Route::get('currency/{currency}', [MainController::class, 'changeCurrency'])->na
 
 Route::group(['middleware' => 'locale'],function(){
 
-require __DIR__.'/auth.php';
+    require __DIR__.'/auth.php';
 
 
 
-Route::get('/', [MainController::class, 'index'])->name('index');
-Route::post('/{product}', [SubscriptController::class, 'store'])->name('subscript');
+    Route::get('/', [MainController::class, 'index'])->name('index');
+    Route::post('/{product}', [SubscriptController::class, 'store'])->name('subscript');
 
-Route::middleware(['auth'])->group(function(){
-    Route::group(['prefix'=>'account'], function(){
-        Route::get('/', [App\Http\Controllers\User\MainController::class, 'index'])->name('user');
-        Route::resource('orders', App\Http\Controllers\User\OrderController::class);
+    Route::middleware(['auth'])->group(function(){
+        Route::group(['prefix'=>'account'], function(){
+            Route::get('/', [App\Http\Controllers\User\MainController::class, 'index'])->name('user');
+            Route::resource('orders', App\Http\Controllers\User\OrderController::class);
+        });
+        Route::group(['prefix'=>'admin', 'middleware'=>['admin']], function(){
+            Route::get('/', [App\Http\Controllers\Admin\MainController::class, 'index'])->name('admin');
+            Route::get('/orders', [App\Http\Controllers\Admin\OrderController::class, 'index'])->name('admin.orders');
+            Route::get('/order/{order}', [App\Http\Controllers\Admin\OrderController::class, 'show'])->name('admin.order.show');
+            Route::resource('categories', App\Http\Controllers\Admin\CategoryController::class);
+            Route::resource('products', App\Http\Controllers\Admin\ProductController::class);
+        });
     });
-    Route::group(['prefix'=>'admin', 'middleware'=>['admin']], function(){
-        Route::get('/', [App\Http\Controllers\Admin\MainController::class, 'index'])->name('admin');
-        Route::get('/orders', [App\Http\Controllers\Admin\OrderController::class, 'index'])->name('admin.orders');
-        Route::get('/order/{order}', [App\Http\Controllers\Admin\OrderController::class, 'show'])->name('admin.order.show');
-        Route::resource('categories', App\Http\Controllers\Admin\CategoryController::class);
-        Route::resource('products', App\Http\Controllers\Admin\ProductController::class);
-    });
-});
 
 
-Route::group(['prefix'=>'bascet'], function(){
-    Route::post('/add/{product}', [BasketController::class, 'bascetAdd'])->name('bascet-add');
-    Route::group(['middleware' => 'bascetNotEmpty'],function(){
-        Route::get('/', [BasketController::class, 'bascet'])->name('bascet');
-        Route::get('/order', [BasketController::class, 'order'])->name('order');
-        Route::post('/remove/{product}', [BasketController::class, 'bascetRemove'])->name('bascet-remove');
-        Route::post('/order', [BasketController::class, 'bascetConfirm'])->name('bascet-confirm');
-    });
+    Route::group(['prefix'=>'bascet'], function(){
+        Route::post('/add/{product}', [BasketController::class, 'bascetAdd'])->name('bascet-add');
+        Route::group(['middleware' => 'bascetNotEmpty'],function(){
+            Route::get('/', [BasketController::class, 'bascet'])->name('bascet');
+            Route::get('/order', [BasketController::class, 'order'])->name('order');
+            Route::post('/remove/{product}', [BasketController::class, 'bascetRemove'])->name('bascet-remove');
+            Route::post('/order', [BasketController::class, 'bascetConfirm'])->name('bascet-confirm');
+        });
     
-});
+    });
 
-Route::group(['prefix' => 'categories'], function(){
-    Route::get('/', [MainController::class, 'categories'])->name('categories');
-    Route::get('/{category}', [MainController::class, 'category'])->name('category');
-    Route::get('/{category}/{product}', [MainController::class, 'product'])->name('product');
-});
+    Route::group(['prefix' => 'categories'], function(){
+        Route::get('/', [MainController::class, 'categories'])->name('categories');
+        Route::get('/{category}', [MainController::class, 'category'])->name('category');
+        Route::get('/{category}/{product}', [MainController::class, 'product'])->name('product');
+    });
 
 });
